49ca09416b283c16dccd3cb28cbdb24b40536411
siraj-io <siraj.khan@harness.io>
2025-02-06 18:44:30 +0530

    user addition for saml login

--- data/scripts/user_provision.sh
+++ data/scripts/user_provision.sh
@@ -1,20 +1,26 @@
 
 # inputs
 
-url=http://<ip>
+url=https://<ip>
 username=<username> # should be part of harness usergroup
 password=<password>
+localLogin=false
+# For localLogin=true enable FF : AUTO_ACCEPT_SAML_ACCOUNT_INVITES to add users without invitation
+# Note: Users added with SAML enabled cannot set user password
 
 userCount=1 # no of users needs to be provisioned
 new_email_id_prefix="harness_perftest_"
 new_email_id_domain="@test.com"
 new_user_password="<random_password>"
 
-
+loginPath=login
+if [ "$localLogin" == "true" ]; then
+  loginPath=harness-local-login
+fi
 
 # login 
 b64Str=$(printf "%s" "$username:$password" | base64)
-login_result_response=$(curl -s --location "$url/gateway/api/users/login" \
+login_result_response=$(curl -s --location "$url/gateway/api/users/$loginPath" \
 --header 'content-type: application/json' \
 --data "{\"authorization\":\"Basic $b64Str\"}" \
 -w "\n%{http_code}\n")
@@ -70,62 +76,63 @@ for ((index = 0; index < $userCount; index++)); do
         exit -1
     fi
 
+    if [ "$localLogin" != "true" ]; then
+        ##
+        search_urlfinal="${search_url}${email}"
+        search_user_response=$(curl -s --location --request POST "$search_urlfinal" \
+                --header "authorization: Bearer $token" \
+                --header "$content_type" \
+                -w "\n%{http_code}\n")    
+        status_code=$(echo "$search_user_response" | awk 'END {print $0}')
 
-    ##
-    search_urlfinal="${search_url}${email}"
-    search_user_response=$(curl -s --location --request POST "$search_urlfinal" \
-            --header "authorization: Bearer $token" \
-            --header "$content_type" \
-            -w "\n%{http_code}\n")    
-    status_code=$(echo "$search_user_response" | awk 'END {print $0}')
+        if [ $status_code -ne 200 ]; then
+            echo "SEARCH USER REQUEST FAILED.."
+            echo $search_user_response
+            exit -1
+        fi
+    
+        invite_id_from_search=$(echo "$search_user_response" | grep -o '"id":"[^"]*' | cut -d '"' -f4)
 
-    if [ $status_code -ne 200 ]; then
-        echo "SEARCH USER REQUEST FAILED.."
-        echo $search_user_response
-        exit -1
+
+        ##
+        inviteFinal="${inviteUrl}${invite_id_from_search}"    
+        get_invite_id_response=$(curl -s --location --request GET "$inviteFinal" \
+                --header "authorization: Bearer $token" \
+                --header "$content_type" \
+                -w "\n%{http_code}\n")
+        status_code=$(echo "$get_invite_id_response" | awk 'END {print $0}')
+
+        if [ $status_code -ne 200 ]; then
+            echo "GET INVITE ID REQUEST FAILED.."
+            echo $get_invite_id_response
+            exit -1
+        fi
+
+        data_value=$(echo "$get_invite_id_response" | grep -o '"data":"[^"]*' | cut -d ':' -f2- | cut -d '"' -f2)
+        token_from_url=$(echo "$data_value" | grep -o 'token=[^&]*' | cut -d '=' -f2)
+    
+
+        ##
+        signup_user_response=$(curl -s --location --request PUT "$signupUrl" \
+                --header "authorization: Bearer $token" \
+                --header "Content-Type: application/json" \
+                --data-raw '{
+                    "email":"'"$email"'",
+                    "name":"Harness User",
+                    "password":"'"$new_user_password"'",
+                    "token":"'"$token_from_url"'",
+                    "accountId":"'"$accountId"'"
+                }' \
+                -w "\n%{http_code}\n")
+        status_code=$(echo "$signup_user_response" | awk 'END {print $0}')
+    
+        if [ $status_code -ne 200 ]; then
+            echo "SIGNUP USER REQUEST FAILED.."
+            echo $signup_user_response
+            exit -1
+        fi
+
+        echo "Signup response for $email: $signup_user_response"
     fi
- 
-    invite_id_from_search=$(echo "$search_user_response" | grep -o '"id":"[^"]*' | cut -d '"' -f4)
-
-
-    ##
-    inviteFinal="${inviteUrl}${invite_id_from_search}"    
-    get_invite_id_response=$(curl -s --location --request GET "$inviteFinal" \
-            --header "authorization: Bearer $token" \
-            --header "$content_type" \
-            -w "\n%{http_code}\n")
-    status_code=$(echo "$get_invite_id_response" | awk 'END {print $0}')
-
-    if [ $status_code -ne 200 ]; then
-        echo "GET INVITE ID REQUEST FAILED.."
-        echo $get_invite_id_response
-        exit -1
-    fi
-
-    data_value=$(echo "$get_invite_id_response" | grep -o '"data":"[^"]*' | cut -d ':' -f2- | cut -d '"' -f2)
-    token_from_url=$(echo "$data_value" | grep -o 'token=[^&]*' | cut -d '=' -f2)
- 
-
-    ##
-    signup_user_response=$(curl -s --location --request PUT "$signupUrl" \
-            --header "authorization: Bearer $token" \
-            --header "Content-Type: application/json" \
-            --data-raw '{
-                "email":"'"$email"'",
-                "name":"Harness User",
-                "password":"'"$new_user_password"'",
-                "token":"'"$token_from_url"'",
-                "accountId":"'"$accountId"'"
-            }' \
-            -w "\n%{http_code}\n")
-    status_code=$(echo "$signup_user_response" | awk 'END {print $0}')
- 
-    if [ $status_code -ne 200 ]; then
-        echo "SIGNUP USER REQUEST FAILED.."
-        echo $signup_user_response
-        exit -1
-    fi
-
-    echo "\nSignup response for $email: $signup_user_response\n"
  
 done
